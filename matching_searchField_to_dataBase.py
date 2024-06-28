9e35ffb9f88706c58cfb93b631bd4ba7475325ad
Samuel Windell <swindell4@ivytech.edu>
2024-06-28 11:30:31 -0400

    Created the searching feature.
    This is the file that will be used for matching data within the search field with data within the database.

---
+++ matching_searchField_to_dataBase.py
@@ -0,0 +1,10 @@
+import re 
+def matchingData(): #This function will be used to match data from the search field to the data in the database. 
+    del results #This makes sure that precious memory is not being wasted. 
+    results = [] #This list ill contain all the matchins results to the data within the search field. 
+    search = searchField.get() #This will capture th current data in the search field. 
+    for data in database.db: #This loop will iterate through the contents of the database and 
+        if re.match(data, search): #This if statement checks to see if the currently observed database element has the same data as what 
+#currently resides in the serch field. 
+            results.append(data) #This adds that data to a list called "results". 
+    return results #This returns the list to be used in another file for showing the list in a text field for the user. 
