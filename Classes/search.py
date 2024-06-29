7e844439f48debd5a9abdfba6fa199c2eb46d0c4
tlindquist70 <117606894+tlindquist70@users.noreply.github.com>
2024-06-28 20:20:58 -0400

    Update search.py

--- Classes/search.py
+++ Classes/search.py
@@ -2,7 +2,7 @@ import sqlite3
 
 #sqlite for temporary purposes
 #find out how django works with sqlite
-#   no me gusta leer documentación
+#   no me gusta leer documentación  <-- hilarious, dave :) -Tracie
 
 def search_customers(search_term: str) -> list:
     conn: sqlite3.Connection = sqlite3.connect('customer_management.db')
