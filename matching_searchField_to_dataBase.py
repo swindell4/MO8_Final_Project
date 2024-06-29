42fef1d105a15558b6424e34d78b864ec7414037
Samuel Windell <swindell4@ivytech.edu>
2024-06-28 22:00:05 -0400

    Made minor tweaks

--- matching_searchField_to_dataBase.py
+++ matching_searchField_to_dataBase.py
@@ -1,6 +1,5 @@
 import re 
 def matchingData(): #This function will be used to match data from the search field to the data in the database. 
-    del results #This makes sure that precious memory is not being wasted. 
     results = [] #This list ill contain all the matchins results to the data within the search field. 
     search = searchField.get() #This will capture th current data in the search field. 
     for data in database.db: #This loop will iterate through the contents of the database and 
