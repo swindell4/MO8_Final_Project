25498dbe3507ea7fcecd56498ea977e18662d2be
nnatsd12349 <123189691+nnatsd12349@users.noreply.github.com>
2024-07-04 06:41:32 -0400

    Update search.py

--- Classes/search.py
+++ Classes/search.py
@@ -1,8 +1,31 @@
 import sqlite3
+import re
+import RepairMaintenanceOptions
 
-#sqlite for temporary purposes
-#find out how django works with sqlite
-#   no me gusta leer documentación  <-- hilarious, dave :) -Tracie
+'''
+matchingData function gets results from  what was typed
+'''
+def matchingData(search):
+    results = []
+    for option in repair_options:
+        if re.match(f".*{search}.*", option, re.IGNORECASE):
+            results.append(option)
+    return results
 
+'''
+updateTextField() updates text field with the matched results of matchingData()
+'''
+def updateSearch():
+    search = searchField.get()
+    matches = matchingData(search)
+    textField.config(state=NORMAL)
+    textField.delete(1.0, END)
+    for match in matches:
+        textField.insert(END, match + "\n")
+    textField.config(state=DISABLED)
+
+'''
+searching customer data
+'''
 def search_customers(search_term: str) -> list:
     conn: sqlite3.Connection = sqlite3.connect('customer_management.db')
